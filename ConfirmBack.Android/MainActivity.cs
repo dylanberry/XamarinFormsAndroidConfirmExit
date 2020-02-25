11f5ebd23420ca11be9b4f5121cbb988f6a8d019
Dylan Berry <dylanberry@gmail.com>
2020-02-25 07:32:35 -1000

    Initial commit

---
+++ ConfirmBack.Android/MainActivity.cs
@@ -0,0 +1,48 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.OS;
+
+namespace ConfirmBack.Droid
+{
+    [Activity(Label = "ConfirmBack", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public override void OnBackPressed()
+        {
+            if (((ConfirmBack.App)App.Current).PromptToConfirmExit)
+            {
+                using (var alert = new AlertDialog.Builder(this))
+                {
+                    alert.SetTitle("Confirm Exit?");
+                    alert.SetMessage("Are you sure you want to exit?");
+                    alert.SetPositiveButton("Yes", (sender, args) => { FinishAffinity(); }); // inform Android that we are done with the activity
+                    alert.SetNegativeButton("No", (sender, args) => {}); // do nothing
+
+                    var dialog = alert.Create();
+                    dialog.Show();
+                }
+                return;
+            }
+            base.OnBackPressed();
+        }
+    }
+}
---
+++ ConfirmBack/App.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using ConfirmBack.Services;
+using ConfirmBack.Views;
+
+namespace ConfirmBack
+{
+    public partial class App : Application
+    {
+        public bool PromptToConfirmExit
+        {
+            get
+            {
+                bool promptToConfirmExit = false;
+                if (MainPage is ContentPage)
+                {
+                    promptToConfirmExit = true;
+                }
+                else if (MainPage is NavigationPage mainPage)
+                {
+                    if (mainPage.CurrentPage is TabbedPage tabbedPage
+                        && tabbedPage.CurrentPage is NavigationPage navigationPage)
+                    {
+                        promptToConfirmExit = navigationPage.Navigation.NavigationStack.Count <= 1;
+                    }
+                    else
+                    {
+                        promptToConfirmExit = mainPage.Navigation.NavigationStack.Count <= 1;
+                    }
+                }
+                else if (MainPage is TabbedPage tabbedPage
+                    && tabbedPage.CurrentPage is NavigationPage navigationPage)
+                {
+                    promptToConfirmExit = navigationPage.Navigation.NavigationStack.Count <= 1;
+                }
+                return promptToConfirmExit;
+            }
+        }
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
